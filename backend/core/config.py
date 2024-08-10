5027df62a624b08dc2ef2e26e8f9409bcdd5da7c
Brian Retamar <brianretamar0101@gmail.com>
2024-08-10 17:05:42 -0300

    created health router

---
+++ backend/core/config.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from pydantic_settings import BaseSettings
+
+class EnvironmentType(str, Enum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    TEST = "test"
+
+class BaseConfig(BaseSettings):
+    class Config:
+        case_sensitive = True
+
+class Config(BaseConfig):
+    DEBUG: int = 1
+    DEFAULT_LOCALE: str = "en_US"
+    ENVIRONMENT: EnvironmentType = EnvironmentType.DEVELOPMENT
+    SQLITE_URL: str = "sqlite:///./test.db"
+    RELEASE_VERSION: str = "0.1"
+    SHOW_SQL_ALCHEMY_QUERIES: int = 0
+
+config: Config = Config()
--- backend/main.py
+++ backend/main.py
@@ -1,45 +1,32 @@
-from fastapi import FastAPI, HTTPException
-from typing import List
-from app.schemas.election import ElectionRequest, ElectionResult
+import uvicorn
 
-app = FastAPI(
-    title="FastAPI D'Hondt Seat Allocation",
-    description="API para calcular la distribución de escaños utilizando el sistema D'Hondt.",
-    version="0.0.1",
-    contact={
-        "name": "Brian",
-        "email": "brianretamar0101@gmail.com",
-    },
-    license_info={
-        "name": "MIT",
-    },
-)
+if __name__ == "__main__":
+    uvicorn.run(
+        app="core.server:app",
+        reload=True,
+        workers=1,
+    )
 
 
-@app.get("/healthcheck")
-def healthcheck():
-    return {"status": "healthy"}
-
-
-@app.post("/calculate_seats/", response_model=ElectionResult)
-async def calculate_seats(election_request: ElectionRequest):
-    seats = election_request.seats
-    votes = election_request.votes
+# @app.post("/calculate_seats/", response_model=ElectionResult)
+# async def calculate_seats(election_request: ElectionRequest):
+#     seats = election_request.seats
+#     votes = election_request.votes
     
-    if seats <= 0:
-        raise HTTPException(status_code=400, detail="Number of seats must be greater than zero")
-    if not votes:
-        raise HTTPException(status_code=400, detail="Votes cannot be empty")
-    if not all(isinstance(v, int) and v >= 0 for v in votes):
-        raise HTTPException(status_code=400, detail="All elements in 'votes' must be non-negative integers")
+#     if seats <= 0:
+#         raise HTTPException(status_code=400, detail="Number of seats must be greater than zero")
+#     if not votes:
+#         raise HTTPException(status_code=400, detail="Votes cannot be empty")
+#     if not all(isinstance(v, int) and v >= 0 for v in votes):
+#         raise HTTPException(status_code=400, detail="All elements in 'votes' must be non-negative integers")
 
-    results = calculate_dhondt(seats, votes)
-    return ElectionResult(results=results)
+#     results = calculate_dhondt(seats, votes)
+#     return ElectionResult(results=results)
 
-def calculate_dhondt(seats: int, votes: List[int]) -> List[int]:
-    allocation = [0] * len(votes)
-    for _ in range(seats):
-        quotients = [votes[i] / (allocation[i] + 1) for i in range(len(votes))]
-        winner = quotients.index(max(quotients))
-        allocation[winner] += 1
-    return allocation
+# def calculate_dhondt(seats: int, votes: List[int]) -> List[int]:
+#     allocation = [0] * len(votes)
+#     for _ in range(seats):
+#         quotients = [votes[i] / (allocation[i] + 1) for i in range(len(votes))]
+#         winner = quotients.index(max(quotients))
+#         allocation[winner] += 1
+#     return allocation
