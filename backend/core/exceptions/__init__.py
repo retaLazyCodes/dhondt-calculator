45891d8545725a6bf342722d08ed2d790dfe79ff
Brian Retamar <brianretamar0101@gmail.com>
2024-08-10 22:19:39 -0300

    custom exception handler

---
+++ backend/core/exceptions/__init__.py
@@ -0,0 +1,15 @@
+from .base import (
+    BadRequestException,
+    CustomException,
+    DuplicateValueException,
+    NotFoundException,
+    UnprocessableEntity,
+)
+
+__all__ = [
+    "BadRequestException",
+    "CustomException",
+    "DuplicateValueException",
+    "NotFoundException",
+    "UnprocessableEntity",
+]
--- backend/core/server.py
+++ backend/core/server.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from api import router
+from core.exceptions import CustomException
 from core.config import config
 
 
 def init_routers(app_: FastAPI) -> None:
     app_.include_router(router)
+
+def init_listeners(app_: FastAPI) -> None:
+    @app_.exception_handler(CustomException)
+    async def custom_exception_handler(request: Request, exc: CustomException):
+        return JSONResponse(
+            status_code=exc.code,
+            content={"error_code": exc.error_code, "message": exc.message},
+        )
 
 
 def create_app() -> FastAPI:
@@ -24,6 +34,7 @@ def create_app() -> FastAPI:
         redoc_url=None if config.ENVIRONMENT == "production" else "/redoc",
     )
     init_routers(app_=app_)
+    init_listeners(app_=app_)
     return app_
 
 
